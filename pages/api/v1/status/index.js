dd5e1658fc1648251538b48b2e8e7ff908a46652
Gabriel Klemtz <166331551+gabrielklemtz01@users.noreply.github.com>
2025-02-21 21:01:10 +0000

    finaliza o endpoint da rota /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,30 @@
 import database from "infra/database.js";
 
 export default async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  res.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString(); // Data no formato ISO 8601
+
+  const versionPost = await database.query("SHOW server_version;");
+  const versionPostValue = versionPost.rows[0].server_version;
+
+  const userMax = await database.query("SHOW max_connections;");
+  const userMaxValue = userMax.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const userUp = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const userUpValue = userUp.rows[0].count;
+
+  res.status(200).json({
+    updated_At: updatedAt,
+    dependecies: {
+      database: {
+        user_Up: parseInt(userUpValue),
+        user_Max: parseInt(userMaxValue),
+        version: versionPostValue,
+      },
+    },
+  });
 }
--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("Get para api/v1/status deve retornar 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
+
+  const resBody = await res.json();
+
+  const parseDate = new Date(resBody.updated_At).toISOString();
+  expect(resBody.updated_At).toEqual(parseDate);
+
+  expect(resBody.dependecies.database.version).toBe("16.7");
+
+  expect(resBody.dependecies.database.user_Max).toBe(100);
+
+  expect(resBody.dependecies.database.user_Up).toEqual(1);
 });
