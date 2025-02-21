32e1b97716718afea31a066c9ed87757dab1b74f
Gabriel Klemtz <166331551+gabrielklemtz01@users.noreply.github.com>
2025-02-21 21:00:27 +0000

    adicionado tratamento de erro abertura/fechamento db

--- infra/database.js
+++ infra/database.js
@@ -8,10 +8,16 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
-  const res = await client.query(queryObject);
-  await client.end();
-  return res;
+
+  try {
+    await client.connect();
+    const res = await client.query(queryObject);
+    return res;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
